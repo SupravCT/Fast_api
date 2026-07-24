212d445a296bccc83ea68099841b3adbbfc0bc1e
Suprav <supravchand2@gmail.com>
2026-07-24 15:06:24 +0545

    jwt

--- src/auth/utils.py
+++ src/auth/utils.py
@@ -1,9 +1,14 @@
 from passlib.context import CryptContext
+from datetime import timedelta,datetime
+import jwt
+from src.config import settings
 
 password_context=CryptContext(
     schemes=['bcrypt']
 
 )
+
+
 
 def generate_password_hash(password:str):
     hash=password_context.hash(password)
@@ -12,3 +17,17 @@ def generate_password_hash(password:str):
 
 def verify_passsword(password:str,hash_password:str):
     return password_context.verify(password,hash_password)
+
+def create_access_token(user_data,expiry:timedelta=timedelta(hours=1)):
+
+    payloadd={}
+    payloadd['user']=user_data
+    payloadd['exp']=datetime.now()+expiry
+
+    token=jwt.encode(
+        payload=payloadd,
+        key=settings.JWT_SECRET_KEY,
+        algorithm=settings.JWT_ALGORITHM
+    )
+
+    return token
