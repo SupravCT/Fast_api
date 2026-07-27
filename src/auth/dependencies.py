b3e27187e1c9d704751ab5e82958f6352ba89dcf
Suprav <supravchand2@gmail.com>
2026-07-27 13:12:01 +0545

    bearer token

---
+++ src/auth/dependencies.py
@@ -0,0 +1,23 @@
+from fastapi.security import HTTPBearer
+from fastapi import Request
+from .utils import decode_token
+from fastapi.exceptions import HTTPException
+
+
+
+class AccessTokenBearer(HTTPBearer):
+
+    def __init__(self,auto_error=True):
+        super().__init__(auto_error=auto_error)
+
+
+    async def __call__(self, request: Request):
+        creds = await super().__call__(request)
+        token = creds.credentials
+
+        token_data = decode_token(token)
+
+        if token_data is None:
+            raise HTTPException(status_code=401, detail="Invalid or expired token")
+
+        return token_data
--- src/books/routes.py
+++ src/books/routes.py
@@ -7,24 +7,31 @@ from src.books.schemas import Book,BookUpdateModel,BookCreateModel
 from src.db.main import get_session
 from sqlmodel.ext.asyncio.session import AsyncSession
 from src.books.service import BookService
+from src.auth.dependencies import AccessTokenBearer
+from uuid import UUID
 
 book_router=APIRouter()
 Book_service=BookService()
+access_token_bearer=AccessTokenBearer()
 
 @book_router.get("/",response_model=list[Book])
-async def get_books(session:AsyncSession=Depends(get_session)):
+async def get_books(session:AsyncSession=Depends(get_session),
+                    user_details=Depends(access_token_bearer)):
     books=await Book_service.get_all_books(session)
     return books
 
 @book_router.post("/",status_code=201)
-async def create_book(book_data:BookCreateModel,session:AsyncSession=Depends(get_session)):
+async def create_book(book_data:BookCreateModel,
+                      session:AsyncSession=Depends(get_session),
+                      user_details=Depends(access_token_bearer)):
 
     new_book=await Book_service.create_book(book_data,session)
 
     return new_book
 
 @book_router.get("/{book_uid}")
-async def get_book(book_uid: int,session:AsyncSession=Depends(get_session)):
+async def get_book(book_uid: UUID,session:AsyncSession=Depends(get_session),
+                   user_details=Depends(access_token_bearer)):
     book=await Book_service.get_book(book_uid,session)
 
     if book:
@@ -34,7 +41,8 @@ async def get_book(book_uid: int,session:AsyncSession=Depends(get_session)):
 
 
 @book_router.patch("/{book_uid}")
-async def update_book(book_uid:int,book_update:BookUpdateModel,session:AsyncSession=Depends(get_session)):
+async def update_book(book_uid:UUID,book_update:BookUpdateModel,session:AsyncSession=Depends(get_session),
+                      user_details=Depends(access_token_bearer)):
     updated_book=await Book_service.update_book(book_uid,book_update,session)
     if updated_book:
         return updated_book
@@ -43,7 +51,8 @@ async def update_book(book_uid:int,book_update:BookUpdateModel,session:AsyncSess
 
 
 @book_router.delete("/{book_uid}")
-async def delete_book(book_uid:int,session:AsyncSession=Depends(get_session)):
+async def delete_book(book_uid:UUID,session:AsyncSession=Depends(get_session),
+                      user_details=Depends(access_token_bearer)):
         book_to_delete=await Book_service.delete_book(book_uid,session)
         if book_to_delete:
             return book_to_delete
