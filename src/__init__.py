9cc7b62b462cae214577f6f5c3b8121fc0ac49df
Suprav <supravchand2@gmail.com>
2026-08-03 09:10:47 +0545

    exception_handle

--- src/__init__.py
+++ src/__init__.py
@@ -3,6 +3,12 @@ from src.books.routes import book_router
 from contextlib import asynccontextmanager
 from src.db.main import init_db
 from src.auth.routes import auth_router
+from .errors import (
+    create_exception_handler,
+    InvaildToken,
+    RevokedToken,
+    NoPermission
+)
 
 
 @asynccontextmanager
@@ -17,6 +23,19 @@ version='v1'
 
 app=FastAPI(
     #lifespan=life_span
+)
+
+app.add_exception_handler(
+    InvaildToken,
+    create_exception_handler(401,"Invalid or expired token")
+)
+app.add_exception_handler(
+    RevokedToken,
+    create_exception_handler(401,"Token has been revoked")
+)
+app.add_exception_handler(
+    NoPermission,
+    create_exception_handler(403,"You do not have permission to perform this action")
 )
 
 app.include_router(book_router,prefix='/api/{version}/books')
--- src/errors.py
+++ src/errors.py
@@ -1,3 +1,6 @@
+from typing import Any,Callable
+from fastapi.requests import Request
+from fastapi.responses import JSONResponse
 
 
 class BookException(Exception):
@@ -11,3 +14,13 @@ class RevokedToken(BaseException):
 
 class NoPermission(BookException):
     pass
+
+
+def create_exception_handler(status_code:int,messages:Any):
+    
+    async def exception_handler(request:Request,exc:BookException):
+        return JSONResponse(
+            status_code=status_code,
+            content={"message": messages}
+        )
+    return exception_handler
